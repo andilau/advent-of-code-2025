a2fbf3bd8a227028eabfe9a651b33d95ed273b6b
Andreas Lau <andilau@users.noreply.github.com>
2025-12-06 18:06:50 +0100

    Day 6: Trash Compactor (#6)
    * Day 6: Trash Compactor
    
    * Day 1: Secret Entrance (Optimize)
    
    * Day 2: Gift Shop (Cleanup)

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -1,5 +1,7 @@
 package days
 
+import java.lang.Math.floorMod
+
 @AdventOfCodePuzzle(
     name = "Secret Entrance",
     url = "https://adventofcode.com/2025/day/1",
@@ -7,28 +9,38 @@ package days
 )
 class Day1(private val input: List<String>) : Puzzle {
 
+    private val positions = 100
+    private val start = 50
+    private val pairs = input.map { it[0] to it.substring(1).toInt() }
+
     override fun partOne(): Int {
-        return input
-            .map { it[0] to it.substring(1).toInt() }
-            .scan(50) { acc, (turn, value) ->
-                when (turn) {
-                    'L' -> (acc - value + 100) % 100
-                    'R' -> (acc + value) % 100
-                    else -> acc
-                }
-            }.count { it == 0 }
+        return pairs.scan(start) { acc, (turn, value) ->
+            when (turn) {
+                'L' -> floorMod(acc - value, positions)
+                'R' -> floorMod(acc + value, positions)
+                else -> acc
+            }
+        }.count { it == 0 }
     }
 
-    override fun partTwo(): Int = input
-            .map { it[0] to it.substring(1).toInt() }
-            .fold(listOf(50)) { acc, (turn, value) ->
-                val last = acc.last()
-                val value1: List<Int> = when (turn) {
-                    'L' -> acc + (last - 1).downTo(last - value)
-                    'R' -> acc + ((last + 1)..(last + value))
-                    else -> acc
+    override fun partTwo(): Int {
+        return sequence {
+            var start = start;
+            yield(start);
+            for ((turn, value) in pairs) {
+                when (turn) {
+                    'L' -> repeat(value) {
+                        start = floorMod(start - 1, positions).also { yield(it) }
+                    }
+
+                    'R' -> repeat(value) {
+                        start = floorMod(start + 1, positions).also { yield(it) }
+                    }
+
+                    else -> throw IllegalArgumentException("Unknown turn $turn")
                 }
-                value1
-            }.count { it % 100 == 0 }
+            }
+        }.count { it == 0 }
+    }
 
 }
